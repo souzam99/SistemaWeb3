87363fcad352b95eeaee1ce98a145ede9facc25a
Marcos Souza <souzam99@gmail.com>
2020-07-14 19:43:30 -0300

    Exception handling for delete (referential integrity)

--- SistemaWeb3/Services/SellerService.cs
+++ SistemaWeb3/Services/SellerService.cs
@@ -58,9 +58,17 @@ namespace SistemaWeb3.Services
 
         public async Task RemoveAsync(int id)
         {
-            var obj = await _context.Seller.FindAsync(id);
-            _context.Seller.Remove(obj);
-            await _context.SaveChangesAsync();
+            try
+            {
+                var obj = await _context.Seller.FindAsync(id);
+                _context.Seller.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                throw new IntegrityException("Don't delete seller because he/she has sales");
+            }
+            
         }
 
         public async Task UpdateAsync(Seller obj)
